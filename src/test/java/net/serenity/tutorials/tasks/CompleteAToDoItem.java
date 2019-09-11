0d0aa09ba42e53da1dd62432866e21207eb1844d
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2019-09-11 23:20:19 +0200

    added: called method on CompleteToDoItem

--- src/test/java/net/serenity/tutorials/tasks/CompleteAToDoItem.java
+++ src/test/java/net/serenity/tutorials/tasks/CompleteAToDoItem.java
@@ -1,13 +1,15 @@
 package net.serenity.tutorials.tasks;
 
+import net.serenitybdd.core.steps.Instrumented;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 
 public class CompleteAToDoItem implements Task {
 
-    public static Performable called(String anItem) {
-        return null;
+    public static CompleteAToDoItem called(String anItem) {
+        return Instrumented.instanceOf(CompleteAToDoItem.class)
+                .withProperties(anItem);
     }
 
     @Override
